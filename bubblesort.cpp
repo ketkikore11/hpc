42b281f8a3dcc087527f8b12f1f0ebef85163b7c
ketkikore11 <100066229+ketkikore11@users.noreply.github.com>
2024-04-23 23:48:32 +0530

    Update bubblesort.cpp

--- bubblesort.cpp
+++ bubblesort.cpp
@@ -1,56 +1,63 @@
 #include <iostream>
 #include <vector>
-#include <omp.h>
+#include <cstdlib>  // For rand()
+#include <ctime>    // For clock()
+#include <omp.h>    // OpenMP
 
-using namespace std;
-
-void bubbleSort(vector<int> &arr) {
-    int n = arr.size();
-    bool swapped;
-    for (int i = 0; i < n - 1; i++) {
-        swapped = false;
-        #pragma omp parallel for shared(arr, swapped)
-        for (int j = 0; j < n - i - 1; j++) {
+// Sequential Bubble Sort
+void bubbleSortSequential(int* arr, int size) {
+    for (int i = 0; i < size - 1; i++) {
+        for (int j = 0; j < size - i - 1; j++) {
             if (arr[j] > arr[j + 1]) {
-                swap(arr[j], arr[j + 1]);
-                swapped = true;
+                int temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
             }
         }
-        // If no two elements were swapped, then the array is sorted
-        if (!swapped)
-            break;
+    }
+}
+
+// Parallel Bubble Sort using OpenMP
+void bubbleSortParallel(int* arr, int size) {
+    #pragma omp parallel for
+    for (int i = 0; i < size - 1; i++) {
+        for (int j = 0; j < size - i - 1; j++) {
+            if (arr[j] > arr[j + 1]) {
+                int temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
     }
 }
 
 int main() {
-    int n;
-    cout << "Enter the number of elements: ";
-    cin >> n;
-
-    vector<int> arr(n);
-    cout << "Enter " << n << " integers:\n";
-    for (int i = 0; i < n; i++) {
-        cin >> arr[i];
+    const int size = 1000;
+    int arr[size];
+    for (int i = 0; i < size; i++) {
+        arr[i] = rand() % 1000;  // Random numbers between 0 and 999
     }
 
-    bubbleSort(arr);
+    // Measure sequential bubble sort time
+    clock_t startSeqBubble = clock();
+    bubbleSortSequential(arr, size);
+    clock_t endSeqBubble = clock();
+    double timeSeqBubble = double(endSeqBubble - startSeqBubble) / CLOCKS_PER_SEC;
 
-    cout << "Sorted array: ";
-    for (int i = 0; i < n; i++) {
-        cout << arr[i] << " ";
-    }
-    cout << endl;
+    // Measure parallel bubble sort time
+    clock_t startParBubble = clock();
+    bubbleSortParallel(arr, size);
+    clock_t endParBubble = clock();
+    double timeParBubble = double(endParBubble - startParBubble) / CLOCKS_PER_SEC;
+
+    // Print execution times
+    std::cout << "Sequential Bubble Sort Time: " << timeSeqBubble << " seconds" << std::endl;
+    std::cout << "Parallel Bubble Sort Time: " << timeParBubble << " seconds" << std::endl;
 
     return 0;
 }
-
 /*
 
-g++ -o bubble -fopenmp bubble.cpp
-./bubble
-
-Enter the number of elements: 5
-Enter 5 integers:
-4 3 6 7 8
-Sorted array: 3 4 6 7 8  
+Sequential Bubble Sort Time: 0.002 seconds
+Parallel Bubble Sort Time: 0.002 seconds
 */
